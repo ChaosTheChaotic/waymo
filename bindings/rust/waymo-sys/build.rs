2af0cf9759fe7bd882fd1851e2a9202acd5e181a
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-01-18 21:37:52 +0000

    Create waymo-sys crate

---
+++ bindings/rust/waymo-sys/build.rs
@@ -0,0 +1,19 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let proot = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).ancestors().nth(3).unwrap().to_path_buf();
+
+    println!("cargo:rustc-link-search=native={}", proot.join("lib/shared").display());
+    println!("cargo:rustc-link-lib=waymo");
+
+    let bindings = bindgen::Builder::default()
+        .header(proot.join("include/waymo/actions.h").display().to_string())
+        .clang_arg(format!("-I{}", proot.join("include").display()))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings.write_to_file(out.join("bindings.rs")).expect("Failed to write bindings");
+}
