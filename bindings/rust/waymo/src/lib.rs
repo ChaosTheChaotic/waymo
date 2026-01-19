59bd189a790303d2b942bf3eae7d68d5d7421817
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-01-19 16:04:25 +0000

    Add builder to proper waymo crate

--- bindings/rust/waymo/src/lib.rs
+++ bindings/rust/waymo/src/lib.rs
@@ -20,14 +20,94 @@ impl From<MouseButton> for wsys::MBTNS {
     }
 }
 
+pub struct EloopParams {
+    inner: *mut wsys::eloop_params,
+}
+
+impl Default for EloopParams {
+    fn default() -> Self {
+        EloopParamsBuilder::new().build()
+    }
+}
+
+impl EloopParams {
+    pub fn new() -> EloopParamsBuilder {
+        EloopParamsBuilder::new()
+    }
+}
+
+impl Drop for EloopParams {
+    fn drop(&mut self) {
+        if !self.inner.is_null() {
+            unsafe {
+                let params = Box::from_raw(self.inner);
+                // We must also reconstruct the CString to free the string buffer
+                if !params.kbd_layout.is_null() {
+                    let _ = CString::from_raw(params.kbd_layout as *mut _);
+                }
+            }
+        }
+    }
+}
+
+pub struct EloopParamsBuilder {
+    max_commands: u32,
+    kbd_layout: String,
+    action_cooldown_ms: u32,
+}
+
+impl EloopParamsBuilder {
+    pub fn new() -> Self {
+        Self {
+            max_commands: 50,
+            kbd_layout: String::from("us"),
+            action_cooldown_ms: 0,
+        }
+    }
+
+    pub fn max_commands(mut self, count: u32) -> Self {
+        self.max_commands = count;
+        self
+    }
+
+    pub fn kbd_layout(mut self, layout: &str) -> Self {
+        self.kbd_layout = layout.to_string();
+        self
+    }
+
+    pub fn action_cooldown_ms(mut self, ms: u32) -> Self {
+        self.action_cooldown_ms = ms;
+        self
+    }
+
+    pub fn build(self) -> EloopParams {
+        let c_layout = CString::new(self.kbd_layout).unwrap();
+        
+        // Allocate the C struct on the heap
+        let inner = Box::into_raw(Box::new(wsys::eloop_params {
+            max_commands: self.max_commands,
+            // Note: into_raw() transfers ownership to the C pointer
+            kbd_layout: c_layout.into_raw(), 
+            action_cooldown_ms: self.action_cooldown_ms,
+        }));
+
+        EloopParams { inner }
+    }
+}
+
+
 pub struct WaymoEventLoop {
     inner: *mut wsys::waymo_event_loop,
 }
 
 impl WaymoEventLoop {
-    pub fn new() -> Option<Self> {
+    pub fn new(params: Option<EloopParams>) -> Option<Self> {
         unsafe {
-            let ptr = wsys::create_event_loop(ptr::null());
+            let p = match params {
+                Some(v) => v.inner,
+                _ => ptr::null()
+            };
+            let ptr = wsys::create_event_loop(p);
             if ptr.is_null() {
                 return None;
             }
